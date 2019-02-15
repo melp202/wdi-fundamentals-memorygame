2ef810712c5aeecb445d6eb947280029a98d781d
Melissa Price <melissaprice@Melissas-Air.fios-router.home>
2019-02-14 19:09:39 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,19 +4,8 @@ var cards = ["queen", "queen", "king", "king"];
 //create cards in play array
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-//add cardOne variable to cardsInPlay array
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1])
-
-
-//check if cardsInPlay length is 2
+function checkForMatch(){
+	//check if cardsInPlay length is 2
 if(cardsInPlay.length === 2){
 	// check if cards match
 	if(cardsInPlay[0] === cardsInPlay[1]){
@@ -25,4 +14,20 @@ if(cardsInPlay.length === 2){
 		alert("Sorry try again");
 	}
 }
+}
+
+function flipCard(cardId){
+console.log("User flipped " + cards[cardId]);
+
+// add flipped card to cardsInPlay array
+cardsInPlay.push(cards[cardId]);
+
+checkForMatch();
+
+}
+
+flipCard(0);
+flipCard(2);
+
+
 
